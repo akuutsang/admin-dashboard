150e3f65c61329e5f832730f3f0a8bb2f85fbaba
akutsang <akuutsang@gmail.com>
2023-08-28 01:30:38 +0100

    add delete icon

--- my-app/src/pages/userList/UserList.jsx
+++ my-app/src/pages/userList/UserList.jsx
@@ -1,47 +1,114 @@
 import './userList.css'
 import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
+import { DeleteOutline } from '@mui/icons-material';
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 90 },
-  {
-    field: 'firstName',
-    headerName: 'First name',
-    width: 150,
-    editable: true,
-  },
-  {
-    field: 'lastName',
-    headerName: 'Last name',
-    width: 150,
-    editable: true,
-  },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 110,
-    editable: true,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-  }
+  { field: 'id', headerName: 'ID', width: 100 },
+  { field: 'user', headerName: 'User', width: 200, renderCell: (params)=>{
+    return ( 
+            <div className='userListUser'>
+              <img className='userListImage' src={params.row.avatar} alt="" /> 
+                {params.row.userName} 
+            </div>
+
+    );
+  } },
+  { field: 'email', headerName: 'Email', width: 200 },
+  { field: 'status', headerName: 'Status', width: 100 },
+  { field: 'transaction', headerName: 'Transaction', width: 100 },
+  { field: 'action', headerName: 'Action', width: 150, renderCell: (params)=>{
+    return(
+      <>
+       <button className='userListEdit'> Edit </button>
+       < DeleteOutline className='userListDelete'/> 
+      </>
+    )
+  }}
+
 ];
 
 const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  { id: 1,
+    userName: 'Jon Snow', 
+    avatar:
+     "./assets/cuteCat.jpg",
+    email:"jon@gmail.com",
+    status:"active",
+    transaction:"$120.00"
+  },
+  { id: 2,
+    userName: 'Jon Snow', 
+    avatar:
+     "./assets/cuteCat.jpg",
+    email:"jon@gmail.com",
+    status:"active",
+    transaction:"$120.00",
+  },
+  { id: 3,
+    userName: 'Jon Snow', 
+    avatar:
+     "./assets/cuteCat.jpg",
+    email:"jon@gmail.com",
+    status:"active",
+    transaction:"$120.00",
+  },
+  { id: 4,
+    userName: 'Jon Snow', 
+    avatar:
+     "./assets/cuteCat.jpg",
+    email:"jon@gmail.com",
+    status:"active",
+    transaction:"$120.00",
+  },
+  { id: 5,
+    userName: 'Jon Snow', 
+    avatar:
+     "./assets/cuteCat.jpg",
+    email:"jon@gmail.com",
+    status:"active",
+    transaction:"$120.00",
+  },
+  { id: 6,
+    userName: 'Jon Snow', 
+    avatar:
+     "./assets/cuteCat.jpg",
+    email:"jon@gmail.com",
+    status:"active",
+    transaction:"$120.00",
+  },
+  { id: 7,
+    userName: 'Jon Snow', 
+    avatar:
+     "./assets/cuteCat.jpg",
+    email:"jon@gmail.com",
+    status:"active",
+    transaction:"$120.00",
+  },
+  { id: 8,
+    userName: 'Jon Snow', 
+    avatar:
+     "./assets/cuteCat.jpg",
+    email:"jon@gmail.com",
+    status:"active",
+    transaction:"$120.00",
+  },
+  { id: 9,
+    userName: 'Jon Snow', 
+    avatar:
+     "./assets/cuteCat.jpg",
+    email:"jon@gmail.com",
+    status:"active",
+    transaction:"$120.00",
+  },
+  { id: 10,
+    userName: 'Jon Snow', 
+    avatar:
+     "./assets/cuteCat.jpg",
+    email:"jon@gmail.com",
+    status:"active",
+    transaction:"$120.00",
+  },
 ];
 
 
@@ -49,14 +116,16 @@ export default function UserList() {
   
   return (
     <div style={{ height: 400, width: '100%' }}>
-      <DataGrid
-        rows={rows}
+      <DataGrid  rows={rows}
+      disableRowSelectionOnClick
         columns={columns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
-        checkboxSelection
-        disableSelectionOnClick
-      />
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 5 },
+          },
+        }}
+        pageSizeOptions={[5, 7, 10]}
+        checkboxSelection  />
     </div>
   )
 }
